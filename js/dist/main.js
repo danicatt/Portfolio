1b33a87b6c320d4a5cff9e890eb37079b5a56aa7
Dani Catterill <danicatterill@gmail.com>
2020-05-09 23:56:51 +0100

    Finished layout and format for toggle navbar.

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,6 +1,17 @@
-const mainNav = document.getElementById('js-menu');
-const navBarToggle = document.getElementById('js-navbar-toggle');
+const mainNav = document.querySelector('.main-nav');
+const navBarToggle = document.querySelector('.navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 440) {
+        document.querySelector('.navbar-toggle').style.backgroundColor = '#ffe401';
+        document.querySelector('.main-nav').style.backgroundColor = '#ffe401';
+    } else {
+    if (window.scrollY <= 439) {
+        document.querySelector('.navbar-toggle').style.backgroundColor = 'transparent';
+        document.querySelector('.main-nav').style.backgroundColor = 'transparent';
+    }
+}});
