e232b5741f19a1b31bd2d22684c6f6b129c10457
Dani Catterill <danicatterill@gmail.com>
2020-05-14 20:47:52 +0100

    menu changes colour on scoll

--- js/dist/main.js
+++ js/dist/main.js
@@ -6,12 +6,12 @@ navBarToggle.addEventListener('click', function () {
 });
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY >= 440) {
+    if (window.scrollY >= 510) {
         document.querySelector('.navbar-toggle').style.backgroundColor = '#ffe401';
         document.querySelector('.main-nav').style.backgroundColor = '#ffe401';
     } else {
-    if (window.scrollY <= 439) {
+    if (window.scrollY <= 509) {
         document.querySelector('.navbar-toggle').style.backgroundColor = 'transparent';
-        document.querySelector('.main-nav').style.backgroundColor = 'transparent';
+        document.querySelector('.main-nav').style.backgroundColor = '#011627';
     }
 }});
