28ac0f6aea938b5cb7be6b0f86c9839c04d1303d
Dani Catterill <danicatterill@gmail.com>
2020-05-18 22:52:52 +0100

    finished final stylings. Improved navbar for better UX

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,18 +1,13 @@
 const mainNav = document.querySelector('.main-nav');
-const navBarToggle = document.querySelector('.navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-   mainNav.classList.toggle('active');
-});
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY >= 510) {
-        navBarToggle.style.backgroundColor = '#bdc0d3';
-        mainNav.style.backgroundColor = '#bdc0d3';
+    if (window.scrollY >= 780) {
+        mainNav.style.display = 'flex';
+        mainNav.style.backgroundColor = '#f0cc98';
+        mainNav.style.justifyContent = 'center';
     } else {
-    if (window.scrollY <= 509) {
-        navBarToggle.style.backgroundColor = 'transparent';
-        mainNav.style.backgroundColor = '#011627';
+    if (window.scrollY <= 779) {
+        mainNav.style.display = 'none';
     }
 }});
 
